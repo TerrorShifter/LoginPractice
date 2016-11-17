fd78d56b893710214cd79f7e733a8a9a00c1cd9f
Jonathan Davis <jonathanedavis@outlook.com>
2016-11-17 01:06:37 -0600

    First commit
    ..

---
+++ LoginPractice/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Jed\Documents\Data.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Role From LoginInfo where Username ='" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows.Count == 1)
+            {
+                this.Hide();
+
+                //possible permission control based on role, otherwise get rid of if/else
+                if(dt.Rows[0][0].ToString() == "Admin")
+                {
+                    Main ss = new Main(dt.Rows[0][0].ToString());
+                    ss.Show();
+                }
+                else
+                {
+                    Main ss = new Main(dt.Rows[0][0].ToString());
+                    ss.Show();
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password");
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+    }
+}
